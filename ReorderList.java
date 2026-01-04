89bebd3e028c4d5ef784a11cc5bb5350451f6491
BALAJI S <balajisubash956@gmail.com>
2026-01-04 11:50:31 +0530

    Learned reordering list(LL)

---
+++ ReorderList.java
@@ -0,0 +1,55 @@
+
+  
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+ 
+
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if(head == null || head.next == null) {
+            return;
+        }
+
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode second = reverseList(slow.next);
+        slow.next = null;
+        ListNode first = head;
+
+        while(second != null) {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
+
+            first.next = second;
+            second.next = tmp1;
+
+            first = tmp1;
+            second = tmp2;
+        }
+
+        
+    }
+
+    public ListNode reverseList(ListNode root) {
+        ListNode prev = null;
+        ListNode curr = root;
+
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
