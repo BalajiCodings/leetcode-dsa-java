7690105ae8654480133a20e69d485edf25631d88
BALAJI S <156514808+BalajiCodings@users.noreply.github.com>
2026-03-06 10:11:59 +0530

    Time: 3 ms (31.26%), Space: 48.6 MB (86.17%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+
+        for(int i = 0; i <= n; i++) {
+            ans[i] = countSetBits(i);
+        }
+
+        return ans;
+    }
+
+    private int countSetBits(int num) {
+        int count = 0;
+
+        while(num != 0) {
+            num = num & num - 1;
+            count++;
+        }
+
+        return count;
+    }
+}
